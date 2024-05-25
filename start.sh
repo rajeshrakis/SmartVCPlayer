447c1eecc773f9b2101cfb9a883733eed2513858
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2024-05-25 16:17:32 +0530

    start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/rajeshrakis/SmartVCPlayer /SmartVCPlayer
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/rajeshrakis/SmartVCPlayer -b $BRANCH /SmartVCPlayer
 fi
-cd /VCPlayerBot
+cd /SmartVCPlayer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
